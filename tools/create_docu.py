f96b317ada66a550908342f3d688d49469e06b1b
Fabian Kalweit <f.kalweit@uib.de>
2020-09-18 14:51:05 +0200

    [chg] add argparse to create docu

--- tools/create_docu.py
+++ tools/create_docu.py
@@ -3,8 +3,41 @@ import os
 import codecs
 import shutil
 
-languages = ["de", "en"]
-outputs = ["html", "pdf"]
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-l", "--lang", type=str, help="languages")
+parser.add_argument("-o", "--outputs", type=str, help="output formates")
+parser.add_argument("-f", "--files", type=str, help="files")
+args = parser.parse_args()
+
+print(args)
+
+languages = []
+if args.lang:
+	for lang in args.lang.split(","):
+			languages.append(lang)
+if len(languages) == 0:
+	languages = ["de", "en"]
+
+outputs = []
+if args.outputs:
+	for output in args.outputs.split(","):
+		outputs.append(output)
+if len(outputs) == 0:
+	outputs == ["html", "pdf"]
+
+input_files = []
+if args.files:
+	for f in args.files.split(","):
+		input_files.append(f)
+
+
+print(languages)
+print(outputs)
+print(input_files)
+
 
 def copy_images(filename, source_dir, destination):
 	img_lines = []
@@ -19,7 +52,7 @@ def copy_images(filename, source_dir, destination):
 
 
 	for f in img_files:
-		print(f"copy {f} to {destination}")
+		# print(f"copy {f} to {destination}")
 		shutil.copy(f"{source_dir}/{f}", destination)
 
 def listdirs(path):
@@ -33,6 +66,8 @@ for lang in languages:
 			continue
 		
 		for f in files:
+			if input_files and os.path.splitext(f)[0] not in input_files:
+				continue
 			root_basename = os.path.basename(root)
 			print(f"{root_basename} --- {os.path.splitext(f)[0]}")
 			if root_basename == os.path.splitext(f)[0]:
@@ -48,14 +83,12 @@ for lang in languages:
 						print ("Successfully created the directory %s " % destination_folder)
 
 					destination = os.path.join(destination_folder, f"{root_basename}.{output}")
-					# if output == "pdf":
-					# 	print(f"asciidoctor -r asciidoctor-pdf -b pdf -a icons=font -a doctype=book -a icons=font -a pdf-style=conf/pdf-theme.yml -a imagesdir=../images -D '{destination_folder}' {f}")
-					# 	os.system(f"asciidoctor -r asciidoctor-pdf -b pdf -a icons=font -a doctype=book -a icons=font -a pdf-style=conf/pdf-theme.yml -a imagesdir=../images -D '{destination_folder}' {os.path.join(root, f)}")					
+					if output == "pdf":
+						print(f"asciidoctor -r asciidoctor-pdf -b pdf -a icons=font -a doctype=book -a icons=font -a pdf-style=conf/pdf-theme.yml -a imagesdir=../images -D '{destination_folder}' {f}")
+						os.system(f"asciidoctor -r asciidoctor-pdf -b pdf -a icons=font -a doctype=book -a icons=font -a pdf-style=conf/pdf-theme.yml -a imagesdir=../images -D '{destination_folder}' {os.path.join(root, f)}")					
 					if output == "html":
+						print(f'asciidoctor -a encoding=UTF-8 -a doctype=book -a icons=font -a xrefstyle=full -a lang={lang} --verbose  --out-file "{destination}" {source} ')
 						os.system(f'asciidoctor -a encoding=UTF-8 -a doctype=book -a icons=font -a xrefstyle=full -a lang={lang} --verbose  --out-file "{destination}" {source} ')
 						copy_images(destination, f"{lang}/images", destination_folder)
 
-		# print("root: %s", root)
-		# print("dirs: %s", dirs)
-		# print("files: %s", files)
-
+		
