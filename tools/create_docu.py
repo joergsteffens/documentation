e4a9ca5a04d93ff151941d13a807be168108675b
Fabian Kalweit <f.kalweit@uib.de>
2020-10-13 16:14:19 +0200

    [chg] add asccidoc opstion to create docu

--- tools/create_docu.py
+++ tools/create_docu.py
@@ -109,15 +109,15 @@ for lang in languages:
 
 					destination = os.path.join(destination_folder, f"{root_basename}.{output}")
 					if output == "pdf":
-						print(f"asciidoctor -r asciidoctor-pdf -b pdf -a icons=font -a doctype=book -a icons=font {pdf_style} -a imagesdir=../images -D '{destination_folder}' {f}")
-						os.system(f"asciidoctor -r asciidoctor-pdf -b pdf -a icons=font -a doctype=book -a icons=font {pdf_style} -a imagesdir=../images -D '{destination_folder}' {os.path.join(root, f)}")					
+						print(f"asciidoctor -r asciidoctor-pdf -b pdf -a icons=font -a doctype=book -chapter-label="" -a icons=font {pdf_style} -a imagesdir=../images -D '{destination_folder}' {f}")
+						os.system(f"asciidoctor -r asciidoctor-pdf -b pdf -a icons=font -a doctype=book -a title-logo-image=../images/opsi_logo.png -a icons=font {pdf_style} -a imagesdir=../images -D '{destination_folder}' {os.path.join(root, f)}")					
 					if output == "html":
-						print(f'asciidoctor -a encoding=UTF-8 -a doctype=book -a icons=font -a xrefstyle=full -a lang={lang} {html_style} --verbose  --out-file "{destination}" {source} ')
+						print(f'asciidoctor -a encoding=UTF-8 -a doctype=book -a icons=font -chapter-label="" -a xrefstyle=full -a lang={lang} {html_style} --verbose  --out-file "{destination}" {source} ')
 						os.system(f'asciidoctor -a encoding=UTF-8 -a doctype=book -a icons=font -a xrefstyle=full -a lang={lang} {html_style} --verbose  --out-file "{destination}" {source} ')
 						copy_images(destination, f"{lang}/images", destination_folder)
 					if output == "epub":
 						shutil.copytree(f"{root}/../images", f"{root}/images")
-						print(f"asciidoctor -r asciidoctor-epub3 -b epub3 -a icons=font -a doctype=book -a icons=font -a imagesdir=images -D '{destination_folder}' {f}")
+						print(f"asciidoctor -r asciidoctor-epub3 -b epub3 -a icons=font -a doctype=book -chapter-label="" -a icons=font -a imagesdir=images -D '{destination_folder}' {f}")
 						os.system(f"asciidoctor -r asciidoctor-epub3 -b epub3 -a icons=font -a doctype=book -a icons=font -a imagesdir=images -D '{destination_folder}' {os.path.join(root, f)}")
 						shutil.rmtree(f"{root}/images")
 		
